d0c4b58632d4c3f35e8a36766fa493997b413bbf
lukeanders70 <luke.larson@berkeley.edu>
2021-08-15 19:10:37 -0700

    [BUG] Pause Game on GameOver Screen

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -14,16 +14,19 @@ public class GameOver : MonoBehaviour
 
     public void OnEnable()
     {
+        Time.timeScale = 0;
         ES.gameObject.SetActive(true);
     }
 
     public void Restart()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void Quit()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("TitleScene");
     }
 }
