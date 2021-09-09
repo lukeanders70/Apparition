c11ea264378e26ac9f8bb99dc857a23a4db2d969
lukeanders70 <luke.larson@berkeley.edu>
2021-09-09 08:57:12 -0700

    [BUG] Fix Empty Rooms Due To Symmetry

--- Assets/Utils/AreaRange.cs
+++ Assets/Utils/AreaRange.cs
@@ -20,13 +20,26 @@ public class AreaRange
         numTilesInRange = System.Math.Max((endX - startX) * (endY - startY), 0);
     }
 
-    public static int GetNumTilesInRanges(AreaRange[] ranges)
+    public static int GetNumTilesInRanges(AreaRange[] ranges, StaticDungeon.Symmetry symmetry, (int, int) center)
     {
         int totalSpawnTiles = 0;
         for (int rangeIndex = 0; rangeIndex < ranges.Length; rangeIndex++)
         {
-            totalSpawnTiles += ranges[rangeIndex].numTilesInRange;
+            totalSpawnTiles += ranges[rangeIndex].getNumberOfTilesInRangeWithSymmetry(symmetry, center);
         }
         return totalSpawnTiles;
+    }
+
+    public (int, int) getMaxValuesWithSymmetry(StaticDungeon.Symmetry symmetry, (int, int) center)
+    {
+        int xMax = symmetry == StaticDungeon.Symmetry.LeftRight || symmetry == StaticDungeon.Symmetry.Quadrant ? Mathf.Min(this.endX, center.Item1 - 1) : this.endX;
+        int yMax = symmetry == StaticDungeon.Symmetry.TopBottom || symmetry == StaticDungeon.Symmetry.Quadrant ? Mathf.Min(this.endY, center.Item2 - 1) : this.endY;
+        return (xMax, yMax);
+    }
+
+    public int getNumberOfTilesInRangeWithSymmetry(StaticDungeon.Symmetry symmetry, (int, int) center)
+    {
+        (int, int) newMaxes = this.getMaxValuesWithSymmetry(symmetry, center);
+        return System.Math.Max((newMaxes.Item1 - startX) * (newMaxes.Item2 - startY), 0);
     }
 }
