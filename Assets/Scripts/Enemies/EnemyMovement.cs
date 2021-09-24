f66cf86dd65d192dde4f05414ca6e4ad57e64a30
lukeanders70 <luke.larson@berkeley.edu>
2021-09-24 07:52:38 -0700

    started

--- Assets/Scripts/Enemies/EnemyMovement.cs
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -7,7 +7,13 @@ public class EnemyMovement : MonoBehaviour
     private float speed;
     [SerializeField]
     private int damage;
+    [SerializeField]
     private Vector3 movement;
+
+    [SerializeField]
+    CircleCollider2D circleCollider;
+
+    private Vector2 lastPosition;
 
     private void Start()
     {
@@ -15,9 +21,33 @@ public class EnemyMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if( lastPosition == rb.position)
+        {
+            forceRaycastBounce();
+        }
+
         rb.velocity = movement * speed;
     }
 
+    void forceRaycastBounce()
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(
+            transform.position + (Vector3)circleCollider.offset,
+            movement,
+            circleCollider.radius + 0.5f
+        );
+        foreach (RaycastHit2D hit in hits)
+        {
+            if ((hit.collider != null) && (hit.collider.gameObject != this.gameObject) && (hit.collider.gameObject.tag != "player"))
+            {
+                Debug.Log(hit.collider.gameObject);
+                Debug.Log(hit.point);
+                movement = Vector3.Reflect(movement, hit.normal);
+                movement.Normalize();
+                break;
+            }
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject collidedObject = collision.collider.gameObject;
@@ -27,10 +57,9 @@ public class EnemyMovement : MonoBehaviour
             if (colliderHealth != null) {
                 colliderHealth.Damage(damage);
             }
-        }
-        else
+        } else
         {
-            movement = Vector3.Reflect(movement, collision.GetContact(0).normal);
+            movement = Vector3.Reflect(movement, );
             movement.Normalize();
         }
     }
